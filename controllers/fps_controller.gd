4204100a8577c5915e70644164345072999f986b
PapaLeech <richard.l.leech@gmail.com>
2026-05-02 17:43:30 +0100

    V4

--- controllers/fps_controller.gd
+++ controllers/fps_controller.gd
@@ -1,21 +1,36 @@
 extends CharacterBody3D
 
-
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+const MOUSE_SENSITIVITY = 0.003
 
+@onready var camera_controller = $CameraController
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	$Terrorist.visible = false
+
+func _input(event):
+	# Quit
+	if event.is_action_pressed("exit"):
+		get_tree().quit()
+	
+	# Mouse look
+	if event is InputEventMouseMotion:
+		rotate_y(-event.relative.x * MOUSE_SENSITIVITY)
+		camera_controller.rotate_x(-event.relative.y * MOUSE_SENSITIVITY)
+		camera_controller.rotation.x = clamp(camera_controller.rotation.x, -1.2, 1.2)
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	# Gravity
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
+	# Jump
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Movement
 	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
