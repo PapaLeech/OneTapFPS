b70105e83f2b8ee4a61f3597035562f5f870b5d7
PapaLeech <richard.l.leech@gmail.com>
2026-05-03 22:21:46 +0100

    CrouchandAimDownSight

--- controllers/fps_controller.gd
+++ controllers/fps_controller.gd
@@ -21,6 +21,15 @@ var _crouch_target_height : float = 1.5
 var _crouch_target_shape : float = 2.0
 const CROUCH_SPEED : float = 10.0
 
+var _is_aiming : bool = false
+@export var fov_default : float = 75.0
+@export var fov_ads : float = 40.0
+@export var ads_speed : float = 10.0
+@export var weapon_hip_pos : Vector3 = Vector3(0.0, 0.0, 0.0)
+@export var weapon_ads_pos : Vector3 = Vector3(-0.19, 0.12, 0.15)
+
+@onready var _weapon_holder : Node3D = $CameraController/Camera3D/WeaponHolder
+
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 @onready var step_handler = $Components/StepHandlerComponent
@@ -61,13 +70,14 @@ func _physics_process(delta):
 
 	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var current_speed = SPEED * 0.6 if _is_crouching else SPEED
 
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * current_speed
+		velocity.z = direction.z * current_speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, current_speed)
+		velocity.z = move_toward(velocity.z, 0, current_speed)
 
 	move_and_slide()
 
@@ -80,6 +90,14 @@ func _physics_process(delta):
 	CAMERA_CONTROLLER.position.y = lerp(CAMERA_CONTROLLER.position.y, _crouch_target_height, t)
 	$CollisionShape3D.position.y = lerp($CollisionShape3D.position.y, _crouch_target_height, t)
 	$CollisionShape3D.shape.height = lerp($CollisionShape3D.shape.height, _crouch_target_shape, t)
+
+	# ADS - smooth FOV zoom and weapon position
+	_is_aiming = Input.is_action_pressed("aim")
+	var target_fov = fov_ads if _is_aiming else fov_default
+	var target_weapon_pos = weapon_ads_pos if _is_aiming else weapon_hip_pos
+	CAMERA_CONTROLLER.fov = lerp(CAMERA_CONTROLLER.fov, target_fov, ads_speed * delta)
+	if _weapon_holder:
+		_weapon_holder.position = _weapon_holder.position.lerp(target_weapon_pos, ads_speed * delta)
 
 	if is_on_floor():
 		step_handler.handle_step_climbing()
