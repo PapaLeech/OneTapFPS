c39c3fc01655ff99c3d5a90978f9d166d4cede6e
PapaLeech <richard.l.leech@gmail.com>
2026-05-30 22:40:22 +0100

    v0.4.x - death anim, hitbox fixes, crosshair hide on death

--- tools/backup_player.gd
+++ tools/backup_player.gd
@@ -2,10 +2,10 @@
 extends EditorScript
 
 func _run():
-	var src = FileAccess.open(\"res://controllers/player.tscn\", FileAccess.READ)
+	var src = FileAccess.open("res://controllers/player.tscn", FileAccess.READ)
 	var content = src.get_as_text()
 	src.close()
-	var dst = FileAccess.open(\"res://controllers/player_BACKUP_pre_mesh_swap.tscn\", FileAccess.WRITE)
+	var dst = FileAccess.open("res://controllers/player_BACKUP_pre_mesh_swap.tscn", FileAccess.WRITE)
 	dst.store_string(content)
 	dst.close()
-	print(\"Backup saved to res://controllers/player_BACKUP_pre_mesh_swap.tscn\")
+	print("Backup saved to res://controllers/player_BACKUP_pre_mesh_swap.tscn")
